9403bf53d35e32aa2fb7042ad3dafd17cef94d8b
RoaaKittaneh <r.kitt29@gmail.com>
2025-11-06 15:52:42 +0200

    Save my latest login page and  updates

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import routes from "./routes";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <>
+      <RouterProvider router={routes} />
+      <ToastContainer position="top-center" autoClose={2000} />
+    </>
+  );
 }
 
 export default App;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,131 @@
-export const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+
+
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch } from "react-redux";
+import { setUserData } from "../store/store";
+
+import "./LoginPage.css";
+
+interface User {
+  id: string;
+  user_name: string;
+  first_name: string;
+  pin: string;
+  birthday: string;
+}
+
+export const LoginPage = () =>  {
+ 
+  //State 
+  
+  const [username, setUsername] = useState("");
+  const [pin, setPin] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { setLogin } = useAuth();
+const dispatch = useDispatch();
+
+   const navigate = useNavigate();
+  
+
+
+  
+  // Login 
+
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+
+
+    if (!username.trim() || !pin.trim()) {
+      setError("please enter user name and PIN");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+     
+      const response = await fetch("https://690a2aaa1a446bb9cc21a097.mockapi.io/users");
+      if (!response.ok) throw new Error("API Error");
+const users = await response.json();
+
+
+    
+      const user = users.find(
+        (u:User) => u.user_name.toLowerCase()===username.trim().toLowerCase()&&u.pin===pin.trim()
+      );
+
+      if (user) {
+      
+  dispatch(setUserData(user));    
+  setLogin(true);     
+      
+        toast.success(
+          ` Welcome back, ${user.first_name || user.user_name}!`,
+          { autoClose: 1600 }
+        );
+
+        
+
+        setTimeout(() => {
+          navigate("/dashboard");
+        }, 700);
+      } else {
+       
+        setError("user name or PIN is incorrect");
+        toast.error( "user name or PIN is incorrect", { autoClose: 2500 });
+      }
+    } catch (err) {
+      console.error(err);
+      setError(" We were unable to connect to the API. Please try again later.");
+      toast.error("API connection failed", { autoClose: 2500 });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  
+  // jsx
+  
+
+  return (
+    <div className="login-container">
+      <h2 className="title">Login</h2>
+      <p className="login-subtitle">Let's get you back to your account!</p>
+      <form onSubmit={handleLogin} className="login-form">
+        <input
+        name="user_name"
+          type="text"
+          placeholder="user name"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+        name="password"
+          type="password"
+          placeholder=" Password"
+          value={pin}
+          onChange={(e) => setPin(e.target.value)}
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Checking ..." : "Login"}
+        </button>
+        {error && <p className="error">{error}</p>}
+      </form>
+
+       
+
+
+    </div>
+  );
+  };
+
+
